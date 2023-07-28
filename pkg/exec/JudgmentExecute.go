bafaaf23bd10e1eb20fe8597967b8beedfa59d9d
Sunshine <guyang_developer@163.com>
2023-07-28 14:52:47 +0800

    add apache license

--- pkg/exec/JudgmentExecute.go
+++ pkg/exec/JudgmentExecute.go
@@ -1,3 +1,20 @@
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package exec
 
 import (
@@ -72,7 +89,7 @@ func (u JudgmentExecute) doWork(scenarioCase *entities.ScenarioCase, conditoinUn
 	}
 
 	scenariorContext.ExecIds = append(scenariorContext.ExecIds, conditoinUnitDesign.Id)
-	log := fmt.Sprintf("[JudgmentExecute] expression:%s , result:%d ,", conditoinUnitDesign.Expr, boolValue)
+	log := fmt.Sprintf("[JudgmentExecute] expression:%s , result:%v ,", conditoinUnitDesign.Expr, boolValue)
 	judgmentContext.AddLogs(log)
 	if boolValue {
 		executeFlow(scenarioCase, conditoinUnitDesign.CorrectBranch, judgmentContext, scenariorContext)
